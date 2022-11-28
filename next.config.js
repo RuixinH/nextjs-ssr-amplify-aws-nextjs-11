bd9d59cc649bfc736a355d4ee9ebdefac70d2a95
nylltz <nylltz@gmail.com>
2022-11-29 09:09:02 +1100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,9 +3,7 @@ module.exports = {
     baseUrl: "https://aws-ssr-pokemon.ibrahimcesar.cloud",
     NEXT_PUBLIC_GA_ID: "G-0H4982YVLL"
   },
-  future: {
-    webpack5: true
-  },
+    webpack5: false,
     images: {
     domains: ['raw.githubusercontent.com'],
   },
