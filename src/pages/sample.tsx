c8f6eaff1799fae470c7c23e3632081fd940c0c0
nylltz <nylltz@gmail.com>
2022-11-29 20:26:22 +1100

    Update sample.tsx

--- src/pages/sample.tsx
+++ src/pages/sample.tsx
@@ -1,17 +1,29 @@
 import Image from 'next/image'
+import mountains from '/public/og.png'
 
-export default function Home() {
-  return (
-    <>
-      <h1>My Homepage</h1>
+const Fill = () => (
+  <div>
+    <div style={{ position: 'relative', width: '300px', height: '500px' }}>
+      <Image alt="Mountains" src={mountains} layout="fill" objectFit="cover" />
+    </div>
+    <div style={{ position: 'relative', width: '300px', height: '500px' }}>
       <Image
-        src="/public/og.png"
-        alt="Picture of the author"
-        width={500}
-        height={500}
-        priority
+        alt="Mountains"
+        src={mountains}
+        layout="fill"
+        objectFit="contain"
       />
-      <p>Welcome to my homepage!</p>
-    </>
-  )
-}
+    </div>
+    <div style={{ position: 'relative', width: '300px', height: '500px' }}>
+      <Image
+        alt="Mountains"
+        src={mountains}
+        layout="fill"
+        objectFit="none"
+        quality={100}
+      />
+    </div>
+  </div>
+)
+
+export default Fill
